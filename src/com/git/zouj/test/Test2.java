132e085378bce40cb94833e453a5969e1e459467
ZheRain <tone1024@163.com>
2018-06-05 16:17:36 +0800

    提交

--- src/com/git/zouj/test/Test2.java
+++ src/com/git/zouj/test/Test2.java
@@ -5,4 +5,24 @@ package com.git.zouj.test;
  * @Date:2018/6/4 18:02
  */
 public class Test2 {
+
+    private String aaaa;
+
+    private Integer bbbb;
+
+    public String getAaaa() {
+        return aaaa;
+    }
+
+    public void setAaaa(String aaaa) {
+        this.aaaa = aaaa;
+    }
+
+    public Integer getBbbb() {
+        return bbbb;
+    }
+
+    public void setBbbb(Integer bbbb) {
+        this.bbbb = bbbb;
+    }
 }
